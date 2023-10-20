a8032b17fe598365dc7568c22ad1cfecf589b5fb
augustosnk12 <augustosnk12@outlook.com>
2023-10-19 22:21:22 -0300

    refactor: remove default files

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export function Header() {
+  return (
+    <div>
+      <h4>GUS</h4>
+
+      <div>
+        <span>Sobre</span> 
+        <span>Trabalhos</span>
+        <span>Contato</span>
+        <span>Icon</span>
+      </div>
+    </div>
+  )
+}
