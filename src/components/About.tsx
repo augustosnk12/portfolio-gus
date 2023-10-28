101d5a7d7484c814ca8ce624d012e155af166e8e
augustosnk12 <augustosnk12@gmail.com>
2023-10-28 11:53:42 -0300

    feat: add contact header

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 export function About() {
-  return (
-    <h3>About</h3>
-  )
+	return (
+		<div>
+			<div className='title-header'>
+				<div className='text-[52px] mt-20 leading-[3.5rem] first-title'>
+					<span>Olá, boa noite!</span> <br />
+					<span>Eu sou o Augusto</span>
+				</div>
+				<span className='grid grid-cols-1 md:grid-cols-2 sm:grid-cols-2 xs:grid-cols-2 mt-3 text-[20px] second-title'>
+					Com 4 anos de experiência, atualmente trabalho desenvolvendo soluções usando a stack do Javascript
+				</span>
+			</div>
+		</div>
+	);
 }
